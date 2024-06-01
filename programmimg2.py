f53be994b4976c2af1f0efe537641a4bdf858e8c
Madhurika2909 <mpriya2909@gmail.com>
2024-06-01 19:10:57 +0530

    Delete programmimg2.py

--- programmimg2.py
+++
@@ -1,7 +0,0 @@
-L=["Sneha","Diya","Riya","Shreya","Tamashree"]
-N=len(L)
-for i in range(0,N):
-    for j in range(0,N-i-1):
-        if(len(L[j])>len(L[j+1])):
-            L[j],L[j+1]=L[j+1],L[j]
-print(L)
